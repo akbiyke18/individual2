57e90f196d73164c24d2df66a4df2d6db0f787be
админ <dzhoroeva_a@auca,kg>
2021-12-19 00:05:04 +0600

    third page commit

---
+++ app/src/main/java/kg/work/individual_2/MainActivityRegister.kt
@@ -0,0 +1,11 @@
+package kg.work.individual_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityRegister : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_register)
+    }
+}
