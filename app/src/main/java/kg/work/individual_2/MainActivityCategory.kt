03c99d12b76f7ae5f84d47a5766bf856b45e60d5
админ <dzhoroeva_a@auca,kg>
2021-12-19 00:40:46 +0600

    fourth page commit

---
+++ app/src/main/java/kg/work/individual_2/MainActivityCategory.kt
@@ -0,0 +1,11 @@
+package kg.work.individual_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityCategory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_category)
+    }
+}
